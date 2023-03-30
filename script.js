f822eaa1e88ba728be5548955f87173b42d8bd44
Morito230 <127374502+Morito230@users.noreply.github.com>
2023-03-30 13:48:03 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//alert("Hola 88888");
+
+function mensaje(){
+    alert("Su mensaje se envio correctamente ")
+     }
+   var nombrecompleto ="Nestor " + "Londero";
+    
+console.log (nombrecompleto)
